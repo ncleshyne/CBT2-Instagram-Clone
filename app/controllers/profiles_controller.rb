cd71f5815f8d24ec10f85331c66d01f242f6d17f
Nicole Shayne <nicoleshayne.elem@gmail.com>
2017-07-27 13:30:54 +0800

    Initial commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,5 +7,19 @@ class ProfilesController < ApplicationController
   @user = User.find_by(user_name: params[:user_name])
   end
   def update
-  end  
+    @user = User.find_by(user_name: params[:user_name])
+    if @user.update(profile_params)
+      flash[:success] = 'Your profile has been updated.'
+      redirect_to profile_path(@user.user_name)
+    else
+      @user.erros.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
+  end
+  private
+
+  def profile_params
+    params.require(:user).permit(:avatar, :bio)
+  end
 end
