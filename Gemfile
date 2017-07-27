83bdda5403b27be1e319ec82f6eb1903c2bc6170
Nicole Shayne <nicoleshayne.elem@gmail.com>
2017-07-27 22:45:02 +0800

    Initial Commit

--- Gemfile
+++ Gemfile
@@ -31,10 +31,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 gem 'simple_form'
 gem 'devise'
-gem 'bcrypt', platforms: :ruby
+gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
 gem 'haml'
 gem 'kaminari', '~> 0.16.3'
 gem 'jquery-turbolinks'
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :set_user  
+  before_action :authenticate_user!
   def show
   @user = User.find_by(user_name: params[:user_name])
   @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
@@ -18,6 +20,18 @@ class ProfilesController < ApplicationController
     end
   end
   private
+
+  def set_user
+  @user = User.find_by(user_name: params[:user_name])
+  end
+
+  def owned_profile
+    @user = User.find_by(user_name: params[:user_name])
+    unless current_user == @user
+      flash[:alert] = "That profile doesn't belong to you!"
+      redirect_to root_path
+    end
+  end
 
   def profile_params
     params.require(:user).permit(:avatar, :bio)
